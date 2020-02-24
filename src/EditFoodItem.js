1c2a8317593eb4ae10ffbe6e9813f5221042f3ff
Triknives <cordell.desu@gmail.com>
2020-02-23 16:25:27 -0800

    update structure

--- src/EditFoodItem.js
+++ src/EditFoodItem.js
@@ -1,22 +1,20 @@
 import React from 'react'
 
-const EditFoodItemForm = (props) => {
+const EditFoodItem = props => {
   return (
-    <form className="col-sm-4">
-      <div>
-        <div>
-          <label className="text-white">Food name</label>
-          <input type="text" name="food" value={props.food} onChange={ props.handleInputChange}/>
-        </div>
-        <div>
-          <label className="text-white">Food cost</label>
-          <input type="number" name="cost" value={props.cost} onChange={ props.handleInputChange} />
-        </div>
-        <button onClick={ props.updateFoodItem }> Update </button>
-        <button onClick={() => props.setEditing(false)}>Cancel</button>
+    <form>
+      <div className="form-group">
+        <label>Food name</label>
+        <input type="text" className="form-control" name="food" value={props.food} onChange={ props.handleInputChange}/>
       </div>
+      <div className="form-group">
+        <label className="text-white">Food cost</label>
+        <input type="number" className="form-control" name="cost" value={props.cost} onChange={ props.handleInputChange} />
+      </div>
+      <button onClick={ props.updateFoodItem } className="btn btn-success mt-2"> Update </button>
+      <button onClick={() => props.setEditing(false)} className="btn btn-info mt-2">Cancel</button>
     </form>
   )
 }
 
-export default EditFoodItemForm;
+export default EditFoodItem;
